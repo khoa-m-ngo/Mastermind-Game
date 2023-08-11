035ec198cb815e2e139304f26c6ac55f6e566df6
khoa-m-ngo <khoa.ngo.m@gmail.com>
2023-08-11 16:49:19 -0400

    added comments

--- mastermind.py
+++ mastermind.py
@@ -1,19 +1,27 @@
 import random
 
+# generate a random number from 1000-9999
 num = random.randrange(1000, 10000)
 print("The answer is: " + str(num))
 
 while(1):
-    n = int(input("Guess the 4 digit number:"))
+    n = input("Guess the 4 digit number: ")
 
-    if n not in range(1000, 10000):
+    # if the input is not a number, end the game
+    if not n.isdigit():
+        break  
+
+    # checking that the input is within range: 1000-9999
+    if int(n) not in range(1000, 10000):
         print("Your guess is out of range")
         continue
     
-    print("Your guess: " + str(n))
+    print("Your guess: " + n)
 
-    if (n == num):
+    # compare input against answer and print the result
+    if (int(n) == num):
         print("Correct answer!")
         break
     else:
         print("Wrong answer!")
+print("Game Finished.")
